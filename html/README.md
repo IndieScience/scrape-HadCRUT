d82f93b091da2a4dc3814a4149ec50f98d1fd992
Gary Wu <dylanwu@live.com>
2021-06-04 15:23:45 -0500

    add README.md in html

---
+++ html/README.md
@@ -0,0 +1,6 @@
+# Web pages from UAE CRU
+
+## Important files
+- index.html - see main
+- 421974/1295957/CRU-Info-sheet-2021.pdf/CRU-Info-sheet-2021.pdf - Main report
+- data.html - List of datasets
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,3 @@
-rm -rf html/*
+shopt -s extglob
+pushd html && rm -rf -- !("README.md") && popd
 wget -P html -p -k -nH --cut-dirs=1 -r -l1 --content-disposition --adjust-extension https://sites.uea.ac.uk/cru/
