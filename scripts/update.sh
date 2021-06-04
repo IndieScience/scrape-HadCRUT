13d17fce40a92b0a71edc0299f6969eb609f5401
Gary Wu <dylanwu@live.com>
2021-06-04 16:04:36 -0500

    capture wget error

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,6 @@
 shopt -s extglob
 pushd html && rm -rf -- !("README.md") && popd
 wget -P html -p -k -nH --cut-dirs=1 -r -l1 -w 0.1 --random-wait --content-disposition --adjust-extension https://sites.uea.ac.uk/cru/
+if [ $? -ne 0 ]; then
+    echo "wget returned: " $?
+fi
