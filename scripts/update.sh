78909b782bbbec8cb3d95c4403ecaac6d979632a
Gary Wu <dylanwu@live.com>
2021-06-04 15:45:01 -0500

    addwait 0.1 to wget

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 shopt -s extglob
 pushd html && rm -rf -- !("README.md") && popd
-wget -P html -p -k -nH --cut-dirs=1 -r -l1 --random-wait --content-disposition --adjust-extension https://sites.uea.ac.uk/cru/
+wget -P html -p -k -nH --cut-dirs=1 -r -l1 -w 0.1 --random-wait --content-disposition --adjust-extension https://sites.uea.ac.uk/cru/
